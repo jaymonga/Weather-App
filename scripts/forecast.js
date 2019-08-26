60096ede75a14de07a2064f5a2e4fde5bfd73ecd
Jay Monga <48458961+jaymonga@users.noreply.github.com>
2019-08-26 11:43:42 -0400

    Create forecast.js

---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const key = "BXakNkrhAegfIYe3qn3QBQmDRI8o8Jez";
+
+// get weath information
+const getWeather = async (id) => {
+
+  const base = "https://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+  const base = "https://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+getCity('New York').then(data => {
+     return getWeather(data.Key);
+    }).then(data => {
+      console.log(data);
+    }).catch(err => console.log(err));
